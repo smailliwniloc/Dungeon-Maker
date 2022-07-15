84585d684c658200d27f17e6b28a82be7dd09d64
smailliwniloc <smailliwniloc@gmail.com>
2022-07-15 16:59:14 -0500

    Added __init__ to project folder

---
+++ project/__init__.py
@@ -0,0 +1,7 @@
+from project.aesthetics import *
+from project.button import *
+from project.client import *
+from project.inputBox import *
+from project.resizable import *
+from project.test2 import *
+from project.test import *
