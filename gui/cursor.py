de17a9f6d8bc49729db6ab10279bcd3bccbe8767
smailliwniloc <smailliwniloc@gmail.com>
2022-07-15 16:52:19 -0500

    Added more stuff

---
+++ gui/cursor.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class Cursor:
+    def __init__(self, imageref):
+        pygame.mouse.set_visible(False)
+        self.image = pygame.image.load(imageref).convert_alpha()
+        
+    def draw(self, surface):
+        surface.blit(self.image, pygame.mouse.get_pos())
---
+++ gui/image.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Image:
+    def __init__(self, image_ref, x, y):
+        self.image = pygame.image.load(image_ref).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self, display):
+        display.blit(self.image, self.rect.topleft)
