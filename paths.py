d6c546dc01cf9271fad158324a9a53cf42ffb631
Colin <smailliwniloc@gmail.com>
2022-07-16 19:16:25 -0500

    Added appropriate paths

---
+++ paths.py
@@ -0,0 +1,17 @@
+import os
+
+dataPath = os.getcwd() + os.sep + "data" + os.sep
+assetPath = os.getcwd() + os.sep + "assets" + os.sep
+
+gamePath = dataPath + "saved" + os.sep
+mapPath = dataPath + "maps" + os.sep
+
+fontPath = assetPath + "fonts" + os.sep
+imagePath = assetPath + "images" + os.sep
+
+tilePath = imagePath + "tiles" + os.sep
+unitPath = imagePath + "units" + os.sep
+
+uiPath = imagePath + "UI" + os.sep  # general, throughout sections.
+uiMenuPath = imagePath + "menu" + os.sep
+uiGamePath = imagePath + "game" + os.sep
